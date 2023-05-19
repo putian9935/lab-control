9a02bca7a59611fea11781c6a9656aacbbd8349a
cqt-strontium <cqt-strontium@outlook.com>
2023-05-19 22:53:24 +0800

    fix host serverd; more robust reconnect behavior

--- lab_control/lab/sr_lab.py
+++ lab_control/lab/sr_lab.py
@@ -8,18 +8,18 @@ AIO0 = AIO(
     minpd=np.array([32953., 32757., 29991., 32786.]),
     maxpd=np.array([34337., 35066., 32848., 33277.]),
     ts_mapping={ramp: 7, hsp: 18})
-AndorCamera = Camera(channel=2)
+# AndorCamera = Camera(channel=2)
 RawTS = TimeSequencer()
 ts_sr = TimeSequencerFPGA('192.168.107.146', 5555)
 fname_gen = FileNameGenerator()
 
-slm = SlaveLockMonitor(
-    r'"C:\Users\strontium_desktop2\miniconda3\python.exe" "C:\Users\strontium_desktop2\Desktop\putian\injection_stabilizer\script\gui.py"')
+# slm = SlaveLockMonitor(
+#     r'"C:\Users\strontium_desktop2\miniconda3\python.exe" "C:\Users\strontium_desktop2\Desktop\putian\injection_stabilizer\script\gui.py"')
 
-wlm_lock = to_sr_remote(WaveLengthMeterLock)(
-    r'"C:\Users\strontium_remote1\AppData\Local\Programs\Python\Python39\python" "C:\Users\strontium_remote1\Desktop\Lock-to-Wave-Meter\ui_multi.py"',
-    name_mapping=['679'],
-    ch_mapping={3: 0})
+# wlm_lock = to_sr_remote(WaveLengthMeterLock)(
+#     r'"C:\Users\strontium_remote1\AppData\Local\Programs\Python\Python39\python" "C:\Users\strontium_remote1\Desktop\Lock-to-Wave-Meter\ui_multi.py"',
+#     name_mapping=['679'],
+#     ch_mapping={3: 0})
 
 oven_c = to_sr_remote(OvenController)(
     r'"C:\Users\strontium_remote1\AppData\Local\Programs\Python\Python39\python" "Q:\strontium\hardware\vacuum_chamber\source_and_viewport_temp_control\oven_control\terminal_software_v3.0.py" > oven_log.txt',
--- script/host_serverd.py
+++ script/host_serverd.py
@@ -2,7 +2,6 @@ import subprocess
 import asyncio
 import socket
 import traceback
-import shlex
 
 
 def check_slave_existence():
@@ -18,7 +17,8 @@ def check_slave_existence():
     proc.wait()
 
 
-def kill_slave(pid):
+def kill_proc(pid):
+    if pid is None: return 
     subprocess.run((rf'taskkill /F /PID {pid}'))
 
 
@@ -28,7 +28,7 @@ class RPyCSlaveDaemon():
         if pid is not None:
             print(
                 f'[INFO] Found slave running with pid {pid}. Killing it.')
-            kill_slave(pid)
+            kill_proc(pid)
         if port is None:
             port = 18816
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,38 +39,32 @@ class RPyCSlaveDaemon():
     async def handle(self, conn, port=None):
         if port is None:
             port = 18812
-        try:
-            with conn:
+        with conn:
+            while True:
                 data = (await self.loop.run_in_executor(None, lambda: conn.recv(100))).strip()
                 if data == b'start':
-                    proc = await asyncio.subprocess.create_subprocess_exec(
-                        shlex.split(rf"C:\Users\strontium_remote1\AppData\Local\Programs\Python\Python39\Python C:\Users\strontium_remote1\Desktop\launch_slave.py --host 0.0.0.0 -p {port}",
+                    await asyncio.subprocess.create_subprocess_shell(
+                        (rf"C:\Users\strontium_remote1\AppData\Local\Programs\Python\Python39\Python C:\Users\strontium_remote1\Desktop\launch_slave.py --host 0.0.0.0 -p {port}"),
                                     stdin=asyncio.subprocess.PIPE
-                                    ))
+                                    )
                     conn.sendall(b'done')
-                elif data == b'stop':
-                    proc.stdin.close()
-                    proc.kill()
-                    conn.sendall(b'done')
-                else:
-                    raise RuntimeError(f"unknown data {data}")
-        except asyncio.CancelledError:
-            proc.stdin.close()
-            proc.kill()
+                    return 
+                elif not len(data):
+                    return 
 
     async def serve(self):
         print('Waiting for connection...')
         task = None
         try:
-            conn, addr = await self.loop.run_in_executor(None, self.s.accept)
-            conn.sendall(b'good')
-            if task is not None:
-                if not task.cancelled() and not task.done():
-                    task.cancel()
-            task = asyncio.create_task(self.handle(conn))
+            while True:
+                conn, addr = await self.loop.run_in_executor(None, self.s.accept)
+                conn.sendall(b'good')
+                kill_proc(check_slave_existence())
+                task = asyncio.create_task(self.handle(conn))
         except Exception:
             traceback.print_exc()
         except:
+            traceback.print_exc()
             if not task.cancelled() and not task.done():
                 task.cancel()
 
