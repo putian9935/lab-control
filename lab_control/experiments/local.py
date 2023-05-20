c9908f6edfbf8ca93f2e761e5b3891078b1f2c5e
putian9935 <pl35@u.nus.edu>
2023-05-21 00:19:53 +0800

    stash files

--- lab_control/experiments/local.py
+++ lab_control/experiments/local.py
@@ -1,13 +1,20 @@
 from lab_control.core.util.unit import *
 from lab_control.core.experiment import Experiment
+from lab_control.core.stage import Stage
 if __name__ == '__main__':
     from ..lab.offline_lab import *
 # --- do not change anything above this line ---
 
-
 @Experiment()
 def main():
-    @sta
-    @RawTS(channel=8, polarity=0)
-    def om_zm_shutter():
-        return []
+    @Stage(start_at=2, duration=7)
+    def stage1():
+        @RawTS(channel=8, polarity=0)
+        def om_zm_shutter():
+            return [1,2,3,4,]
+        
+    @Stage()
+    def stage3():
+        @RawTS(channel=2, polarity=0)
+        def om_zm_shutter():
+            return [5,4]
