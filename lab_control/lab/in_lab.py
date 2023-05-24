702449fbb97adef9126ab1f9e18efd7008313e69
putian9935 <pl35@u.nus.edu>
2023-05-24 11:47:45 +0800

    fix bug

--- lab_control/lab/in_lab.py
+++ lab_control/lab/in_lab.py
@@ -1,6 +1,6 @@
-from device import *
+from lab_control.device import *
 
 
 TSChannel = TimeSequencer()
-ts_in = TimeSequencerFPGA('192.168.107.146', 5555)
+# ts_in = TimeSequencerFPGA('192.168.107.146', 5555)
 # fname_gen = FileNameGenerator()
