12b917ac5a4e5bbef160a5afb9dbb2a9c2d85ab6
jinchao <e0338231@u.nus.edu>
2023-05-25 15:24:50 +0800

    prepare solis mouse control

--- lab_control/device/camera_solis/camera_solis.py
+++ lab_control/device/camera_solis/camera_solis.py
@@ -1,28 +1,14 @@
-from core.target import Target
-from core.action import Action, set_pulse
-import asyncio 
+from ...core.target import Target
+import asyncio
+import rpyc
+
+
 class CameraSolis(Target):
-    def __init__(self, channel) -> None:
-        self.channel = channel 
-        super().__init__() 
+    def __init__(self, fname_gen=None) -> None:
+        super().__init__()
+        hostname = 'CASPERN-6AKOV6A'
+        self.conn = rpyc.connect(hostname, 17733)
+        self.fname_gen = fname_gen 
 
-@set_pulse
-@CameraSolis.set_default
-@CameraSolis.take_note
-class external_purge(Action):
-    async def run_preprocess(self, target):
-        return await asyncio.get_event_loop().run_in_executor(None, input, "Terminate the current (if any) acquisition from Andor Solis, and start a new acquisition with 'external with purge'.\nPress enter when you are done.") 
-    
-    def to_time_sequencer(self, target: CameraSolis) -> tuple[dict[int, list[int]], bool]:
-        return {target.channel: (self.retv, self.polarity, self.signame)}
-
-
-if __name__ == '__main__':
-    cam = CameraSolis(channel=3)
-
-    @cam(external_purge)
-    def trig():
-        return [1, 20000]
-
-    asyncio.run(cam.run_preprocess())
-    print(cam.to_time_sequencer())
+    async def run_preprocess(self):
+        return await asyncio.get_event_loop().run_in_executor(None, self.conn.root.emccd_fnAcq, 'haha')
