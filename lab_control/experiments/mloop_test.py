60a6fa0e98e6e3185838bdf25364c16f4f0fff30
putian9935 <pl35@u.nus.edu>
2023-12-28 11:39:29 +0800

    add pgc b field mloop

--- lab_control/experiments/mloop_test.py
+++ lab_control/experiments/mloop_test.py
@@ -4,15 +4,16 @@ from lab_control.core.experiment import Experiment, at_acq_start
 from lab_control.core.stage import Stage
 from lab_control.core.util.img import get_tif_fp, get_tif_latest_single_image
 from lab_control.core.util.find_waist import parse_single_img 
+import time 
 
 if __name__ == '__main__':
     from ..lab.in_lab import *
-    # from ..lab.offline_lab import *
 import asyncio
 
 import mloop.interfaces as mli
 import mloop.controllers as mlc
-from . import pgc_mloop
+from .pgc_mloop import pgc_mloop
+
 loop = asyncio.get_event_loop()
 
 class TemperatureMinimizer(mli.Interface):
@@ -32,7 +33,9 @@ class TemperatureMinimizer(mli.Interface):
 
     def get_next_cost_dict(self, params_dict):
         params = params_dict['params']
-        loop.create_task(self.coro(*params))
+        tsk = loop.create_task(self.coro(*params))
+        while not tsk.done():
+            time.sleep(1e-1)
 
         fitting_parameter = parse_single_img(get_tif_latest_single_image(self.tif_fp)) 
 
@@ -47,7 +50,7 @@ def mloop_main():
     remote_config.update_cnt()
     fname = remote_config.gen_fname_from_dict({})
     start_acq(fname)
-
+    # loop.create_task(at_acq_start())
     # First create your interface
     interface = TemperatureMinimizer(
         get_tif_fp(fname, remote_config.output_dir),
